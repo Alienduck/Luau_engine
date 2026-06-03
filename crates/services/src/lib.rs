cbf130d03c454b86b3fac0faccfdff81d5fc9fc6
Alienduck <oneuillyr@gmail.com>
2026-06-03 21:34:26 +0200

    replace context action by user input

--- crates/services/src/lib.rs
+++ crates/services/src/lib.rs
@@ -2,5 +2,5 @@
 // pub mod run_service; -- future (Heartbeat, RenderStepped)
 // pub mod tween_service; -- future
 // pub mod lighting; -- future
-pub mod context_action;
 pub mod run_service;
+pub mod user_input;
