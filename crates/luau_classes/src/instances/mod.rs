7cf5dbb86d764671747a91212d03c6044bfddbe8
Owen Neuillyr <jacquonneries@gmail.com>
2026-06-04 20:48:33 +0200

    addd frame and screen gui

--- crates/luau_classes/src/instances/mod.rs
+++ crates/luau_classes/src/instances/mod.rs
@@ -1,7 +1,9 @@
 pub mod base_part;
 pub mod camera;
 pub mod collider;
+pub mod frame;
 pub mod part;
 pub mod rigidbody;
+pub mod screen_gui;
 //TODO: pub mod model; -- future
 //TODO: pub mod workspace; -- future
