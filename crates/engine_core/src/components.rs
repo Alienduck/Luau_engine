9ca9e37f492e4c87ff531921c523a8fab37c9f1b
Alienduck <oneuillyr@gmail.com>
2026-06-22 22:52:22 +0200

    add luau atmosphere

--- crates/engine_core/src/components.rs
+++ crates/engine_core/src/components.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::Component;
+use bevy::{color::Color, prelude::Component};
 
 /// Struct to handle easier the bloom script side
 /// [TODO]: found a way to handle without the custom struct
@@ -7,4 +7,13 @@ pub struct LuauBloom {
     pub intensity: f32,
     pub size: f32,
     pub threshold: f32,
+}
+
+#[derive(Component, Clone)]
+pub struct LuauAtmosphere {
+    pub density: f32,
+    pub color: Color,
+    pub decay: Color,
+    pub glare: f32,
+    pub haze: f32,
 }
