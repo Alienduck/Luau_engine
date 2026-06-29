9658a0ddae2dd6abededa0652c68f8932d2804df
Alienduck <oneuillyr@gmail.com>
2026-06-29 18:07:05 +0200

    replace old fields method by impl base instance fields

--- crates/luau_classes/src/instances/part.rs
+++ crates/luau_classes/src/instances/part.rs
@@ -98,8 +98,7 @@ impl CloneableInstance for LuaPart {
 
 impl UserData for LuaPart {
     fn add_fields<F: UserDataFields<Self>>(fields: &mut F) {
-        fields.add_field_method_get("Name", |_, this| Ok(this.data.base.name.clone()));
-        fields.add_field_method_get("ClassName", |_, this| Ok(this.data.base.class_name));
+        crate::impl_base_instance_fields!(fields);
         fields.add_field_method_get("Position", |_, this| {
             Ok(LuaVector3 {
                 x: this.data.cframe.position.x,
@@ -116,19 +115,8 @@ impl UserData for LuaPart {
                 id: this.data.touched_signal_id,
             })
         });
-        fields.add_field_method_get("Parent", |lua, this| {
-            let Some(parent_handle) = this.data.base.parent_handle else {
-                return Ok(None);
-            };
-            let cache: mlua::Table = lua.named_registry_value("__instance_cache")?;
-            Ok(cache.get::<Option<mlua::AnyUserData>>(parent_handle)?)
-        });
         fields.add_field_method_get("Material", |_, this| Ok(this.data.material.as_str()));
         fields.add_field_method_get("Shape", |_, this| Ok(this.shape));
-        fields.add_field_method_set("Name", |_, this, v: String| {
-            this.data.base.set_name(v);
-            Ok(())
-        });
         fields.add_field_method_set("Position", |_, this, v: LuaVector3| {
             this.data.set_position(v);
             Ok(())
@@ -147,10 +135,6 @@ impl UserData for LuaPart {
         });
         fields.add_field_method_set("Transparency", |_, this, t: f32| {
             this.data.set_transparency(t);
-            Ok(())
-        });
-        fields.add_field_method_set("Parent", |lua, this, parent: Option<mlua::AnyUserData>| {
-            this.data.base.set_parent(lua, parent);
             Ok(())
         });
         fields.add_field_method_set("Material", |_, this, v: String| {
