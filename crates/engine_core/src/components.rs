6353e9e15fe806e63f0a2d85f287378cc42af6e3
Alienduck <oneuillyr@gmail.com>
2026-07-18 18:39:54 +0200

    add missing field walk_speed

--- crates/engine_core/src/components.rs
+++ crates/engine_core/src/components.rs
@@ -21,6 +21,7 @@ pub struct LuauAtmosphere {
 #[derive(Component, Default)]
 pub struct LuauCharacterController {
     pub velocity: Vec3,
+    pub walk_speed: f32,
     pub vertical_velocity: f32,
     pub jump_power: f32,
     pub wants_to_jump: bool,
