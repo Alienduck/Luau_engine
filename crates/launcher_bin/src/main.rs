57ab2fb337614082cfaf0c4441962c6fef444b79
Alienduck <oneuillyr@gmail.com>
2026-08-28 18:21:25 +0200

    simple code to test

--- crates/launcher_bin/src/main.rs
+++ crates/launcher_bin/src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+
 fn main() {
     println!("Hello Launcher!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(launcher_ui::LauncherPlugin)
+        .run();
 }
