790640527e9d6c007cfbd5c2e0fce9d68ea899d4
Alienduck <oneuillyr@gmail.com>
2026-09-21 13:18:34 +0200

    start making the top pannel

--- crates/launcher_ui/src/top_panel.rs
+++ crates/launcher_ui/src/top_panel.rs
@@ -8,6 +8,29 @@ impl Plugin for TopPanel {
     }
 }
 
-fn build(mut commands: Commands) {
-    commands.spawn((Node { ..default() }));
+fn build(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let container = commands
+        .spawn(Node {
+            width: percent(100),
+            height: percent(20),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        })
+        .id();
+
+    let image = commands
+        .spawn((
+            ImageNode::new(asset_server.load("images/banner.png"))
+                .with_mode(NodeImageMode::Stretch),
+            Node {
+                width: percent(100),
+                aspect_ratio: Some(16.0 / 9.0),
+                position_type: PositionType::Absolute,
+                ..default()
+            },
+        ))
+        .id();
+
+    commands.entity(container).add_child(image);
 }
