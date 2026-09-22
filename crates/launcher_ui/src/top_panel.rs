1d115f87b3d735ea440328eb6baeb5b630721329
Alienduck <oneuillyr@gmail.com>
2026-09-22 15:34:34 +0200

    convert into bsn

--- crates/launcher_ui/src/top_panel.rs
+++ crates/launcher_ui/src/top_panel.rs
@@ -1,68 +1,54 @@
 use bevy::prelude::*;
+use bevy::text::FontSourceTemplate;
 
-pub(crate) struct TopPanel;
+pub fn top_panel() -> impl Scene {
+    let banner_aspect_ratio = Some(2560. / 640.);
+    let icon_aspect_ratio = Some(159. / 121.);
+    let banner_padding = UiRect::horizontal(Val::Px(2.)).with_top(Val::Px(5.));
 
-impl Plugin for TopPanel {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, build);
-    }
-}
-
-fn build(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let container = commands
-        .spawn((Node {
+    bsn! {
+        Node {
             width: Val::Percent(100.0),
             height: Val::Vh(25.),
             align_items: AlignItems::Center,
             overflow: Overflow::hidden(),
-            border_radius: BorderRadius::all(Val::Px(10.)),
-            ..default()
-        },))
-        .id();
-
-    let image = commands
-        .spawn((
-            ImageNode::new(asset_server.load("images/banner.png")),
-            Node {
-                position_type: PositionType::Absolute,
-                width: Val::Percent(100.0),
-                aspect_ratio: Some(2560.0 / 640.0),
-                padding: UiRect::horizontal(Val::Px(2.)).with_top(Val::Px(5.)),
-                border_radius: BorderRadius::all(Val::Px(10.0)),
-                ..default()
-            },
-        ))
-        .id();
-
-    let icon = commands
-        .spawn((
-            ImageNode::new(asset_server.load("images/lucid_white_icon.png")),
-            Node {
-                margin: UiRect::left(Val::Px(5.)),
-                height: Val::Percent(80.),
-                aspect_ratio: Some(159. / 121.),
-                ..default()
-            },
-        ))
-        .id();
-
-    let title = commands
-        .spawn((
-            Text::new("Lucid Engine"),
-            TextFont {
-                font: asset_server.load("fonts/montserrat.ttf").into(),
-                font_size: FontSize::Vw(5.),
-                weight: FontWeight(700),
-                ..default()
-            },
-            Node {
-                padding: UiRect::all(Val::Px(5.)),
-                ..default()
-            },
-        ))
-        .id();
-
-    commands.entity(container).add_child(image);
-    commands.entity(container).add_child(icon);
-    commands.entity(container).add_child(title);
+            border_radius: BorderRadius::all(Val::Px(10.))
+        }
+        Children [
+            (
+                ImageNode {
+                    image: "images/banner.png",
+                }
+                Node {
+                    position_type: PositionType::Absolute,
+                    width: Val::Percent(100.),
+                    aspect_ratio: banner_aspect_ratio,
+                    padding: banner_padding,
+                    border_radius: BorderRadius::all(Val::Px(10.))
+                }
+            ),
+            (
+                ImageNode {
+                    image: "images/lucid_white_icon.png",
+                }
+                Node {
+                    margin: UiRect::left(Val::Px(5.)),
+                    height: Val::Percent(80.),
+                    aspect_ratio: icon_aspect_ratio
+                }
+            ),
+            (
+                Text::new("Lucid Engine")
+                TextColor(Color::linear_rgb(10., 10., 10.))
+                TextFont {
+                    font: FontSourceTemplate::Handle("fonts/montserrat.ttf"),
+                    font_size: FontSize::Vw(5.),
+                    weight: FontWeight(700)
+                }
+                Node {
+                    padding: UiRect::all(Val::Px(5.))
+                }
+            )
+        ]
+    }
 }
