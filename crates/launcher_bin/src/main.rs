8fdd46a223ed14c9ea8ab98bcce812186ac0e91d
Alienduck <oneuillyr@gmail.com>
2026-09-22 13:24:11 +0200

    add contraints to the window and new default resolution

--- crates/launcher_bin/src/main.rs
+++ crates/launcher_bin/src/main.rs
@@ -1,9 +1,20 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
 
 fn main() {
     println!("Hello Launcher!");
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resize_constraints: WindowResizeConstraints {
+                    min_height: 260.,
+                    min_width: 480.,
+                    ..default()
+                },
+                resolution: WindowResolution::new(720, 480),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(launcher_ui::LauncherPlugin)
         .run();
 }
